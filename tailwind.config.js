eacb23a5eb4ba14adecfc314ed2926d4d9df0115
3B032104 <3B032104@gm.student.ncut.edu.tw>
2023-12-06 23:44:18 +0800

    安裝tailwindcss及preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "node_modules/preline/dist/*.js",
     ],
-
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+        extend: {},
     },
-
-    plugins: [forms],
-};
+    plugins: [
+        forms,
+        require('preline/plugin'),
+    ],
+}
