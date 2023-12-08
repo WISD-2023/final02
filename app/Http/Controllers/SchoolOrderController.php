792b51c67587c05a4298c31535c81557e4597410
3B032104 <3B032104@gm.student.ncut.edu.tw>
2023-12-08 12:30:42 +0800

    建立以all-in-one生成SchoolOrder所需model, migration, controller, factory

---
+++ app/Http/Controllers/SchoolOrderController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SchoolOrder;
+use App\Http\Requests\StoreSchoolOrderRequest;
+use App\Http\Requests\UpdateSchoolOrderRequest;
+
+class SchoolOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSchoolOrderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SchoolOrder $schoolOrder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SchoolOrder $schoolOrder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSchoolOrderRequest $request, SchoolOrder $schoolOrder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SchoolOrder $schoolOrder)
+    {
+        //
+    }
+}
